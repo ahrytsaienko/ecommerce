ad04d291ff125f9377870a204b84492130a7ab98
temsan89 <temsan89@gmail.com>
2018-01-08 12:20:23 +0200

    contact form, templates, validation

---
+++ src/ecommerce/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import ContactForm
+
+
+def home_page(request):
+    return render(request, 'home_page.html', {'context': 'foobar'})
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        'title': 'Contact',
+        'content': 'Welcome to the contact page.',
+        'form': contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request, 'contact/view.html', context)
