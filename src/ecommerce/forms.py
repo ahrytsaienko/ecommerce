f0debdd17c6a9cb90fa0e94037650f8b2d07a4b6
A.Hrytsaienko <temsan89@gmail.com>
2018-01-08 20:32:00 +0200

    login_page

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -29,3 +29,7 @@ class ContactForm(forms.Form):
             raise forms.ValidationError('Email has to be gmail.com')
         return email
 
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
