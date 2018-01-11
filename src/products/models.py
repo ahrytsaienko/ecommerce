d58734d25cc3077e5da351f615a6d0ea32a38f77
A.Hrytsaienko <temsan89@gmail.com>
2018-01-11 23:33:16 +0200

    prod manager

--- src/products/models.py
+++ src/products/models.py
@@ -1,12 +1,44 @@
+import random
+import os
+
 from django.db import models
 
-# Create your models here.
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 39104124214124)
+    namem, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(
+        new_filename=new_filename,
+        ext=ext
+    )
+    return 'products/{new_filename}/{final_filename}'.format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
+
+
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() ==1
+            return qs.first()
 
 
 class Product(models.Model):
+
+    objects = ProductManager()
+
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    image = models.ImageField(
+        upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
