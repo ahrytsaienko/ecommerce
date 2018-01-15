8886c9886ce6f10ca581233444502cd1fd1a7c00
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-01-15 11:32:32 +0200

    tags with include

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -15,6 +15,7 @@ def contact_page(request):
         'title': 'Contact',
         'content': 'Welcome to the contact page.',
         'form': contact_form,
+        'brand': 'new Brand name'
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
